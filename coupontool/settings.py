d7e2ed91608e8259a7b3bed6c3246a8f92696ef3
rivet4u <88958978+rivet4u@users.noreply.github.com>
2021-09-18 16:00:11 +0400

    apss

--- coupontool/settings.py
+++ coupontool/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-1e8k-*%vh&!ln1kt#kx_3*^n=k=$=2$5&l3pcb$8i!gg#!$-y_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =['coupontool.herokuapp.com'] 
+ALLOWED_HOSTS =['coupontool.herokuapp.com']    
 
 # Application definition
 
