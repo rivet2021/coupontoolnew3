16d46abac85b21aaa8d0e445cb3a681eaafa534d
rivet4u <88958978+rivet4u@users.noreply.github.com>
2021-09-22 15:25:36 +0400

    allowed host change

--- coupontool/settings.py
+++ coupontool/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 'django-insecure-1e8k-*%vh&!ln1kt#kx_3*^n=k=$=2$5&l3pcb$8i!gg#!$-y_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
